6758a959affe7498c3367c3fb1009f15a290558e
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-18 00:33:58 +0530

    Added: Remove Data

--- js/addressBookHome.js
+++ js/addressBookHome.js
@@ -1,4 +1,4 @@
-let addrBookList
+let addrBookList;
 window.addEventListener('DOMContentLoaded', (event) => {
     addrBookList = getAddressBookDataFromStorage();
     document.querySelector(".per-count").textContent = addrBookList.length;
@@ -25,11 +25,23 @@ const createInnerHtml = () => {
                 <td>${addrBookData._zip}</td>
                 <td>${addrBookData._phone}</td>
                 <td>
-                    <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                    <img id="${addrBookData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                     <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
                 </td>
             </tr>
         `;
     }
     document.querySelector('#display').innerHTML = innerHtml;
+}
+
+const remove = (node) => {
+    let addrAddressData = addrBookList.find(addrData => addrData._id == node.id);
+   if(!addrAddressData) return;
+    const index = addrBookList 
+                    .map(addrData => addrData._id)
+                    .indexOf(addrAddressData._id);
+                    addrBookList.splice(index, 1);
+    localStorage.setItem('AddressBookList', JSON.stringify(addrBookList));
+    document.querySelector('.per-count').textContent = addrBookList.length;
+    createInnerHtml();
 }
